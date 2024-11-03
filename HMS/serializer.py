e920aec0fc3c353565c28447f303ae28bd8926f7
sugandhi15 <sugandhibansal26@gmail.com>
2024-11-03 18:07:49 +0530

    added create method to my serializer to enable password saved in hashed manner

--- HMS/serializer.py
+++ HMS/serializer.py
@@ -6,6 +6,17 @@ class Userserializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            account_type=validated_data.get('account_type', 'Customer')
+        )
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
